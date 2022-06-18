ff6cbb00af4cafce79f9c6e8265332db64fea3b7
simpson <simpsonwork@gmail.com>
2022-06-18 12:32:36 +0300

    Handler attributes from CqrsKit package

--- resources/skeleton/CommandHandler.tpl.php
+++ resources/skeleton/CommandHandler.tpl.php
@@ -7,6 +7,8 @@ use <?= $command_fqcn ?>;
 /**
  * @see \<?= "$command_fqcn\n" ?>
  */
+<?php if (class_exists('\Mechanic\CqrsKit\Attribute\CommandHandler'))
+    echo '#[\Mechanic\CqrsKit\Attribute\CommandHandler]', PHP_EOL; ?>
 class <?= "$class_name\n" ?>
 {
     public function __invoke(<?= $command ?> $command)
--- resources/skeleton/QueryHandler.tpl.php
+++ resources/skeleton/QueryHandler.tpl.php
@@ -7,6 +7,8 @@ use <?= $query_fqcn ?>;
 /**
  * @see \<?= "$query_fqcn\n" ?>
  */
+<?php if (class_exists('\Mechanic\CqrsKit\Attribute\QueryHandler'))
+    echo '#[\Mechanic\CqrsKit\Attribute\QueryHandler]', PHP_EOL; ?>
 class <?= "$class_name\n" ?>
 {
     public function __invoke(<?= $query ?> $query)
